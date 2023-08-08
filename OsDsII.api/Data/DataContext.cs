b41bf036272935e17b9a2fe95796bb7b4ffd286d
Etec Prof. Horácio Augusto da Silveira <rafaeltomaz2701@gmail.com>
2023-08-07 22:57:06 -0300

    SegundaAula

--- OsDsII.api/Data/DataContext.cs
+++ OsDsII.api/Data/DataContext.cs
@@ -1,13 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using OsDsII.api.Models;
 
 namespace OsDsII.api.Data
 {
     public class DataContext : DbContext
     {
-        
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {}
+
+        public DbSet<Customer> Custumers { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Customer>()
+                .HasIndex(custumer => custumer.Email)
+                .IsUnique();
+        }
     }
 }
